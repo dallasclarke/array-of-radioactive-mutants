35329cbe7bff9a8c72356ee6d11a80b949821444
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-29 11:47:10 -0700

    lab

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd (arr) {
+  arr.push('Colin');
+}
 
+function addMeToStart (arr) {
+  arr.unshift('Colin');
+}
+
+function changeLast (arr) {
+  arr.pop();
+  
+  
+}
+
+function changeAllValuesTo (arr) {
+
+}
+
+function oddOrEven (arr) {
+  if (arr % 2 === 0) {
+    return "even"
+  } else {
+    return "odd"
+  }
+}
+
+function changeNextThreeToValue (arr) {
+  arr.splice(2, 3,)
+}
 
 
 
